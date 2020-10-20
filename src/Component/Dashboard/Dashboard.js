7a057ff5762ad9fcf6203ddda6c77e9acdbbafcd
nayan.pimpare <npimpare@gmail.com>
2020-10-20 19:10:04 +0530

    Dashboard changes

--- src/Component/Dashboard/Dashboard.js
+++ src/Component/Dashboard/Dashboard.js
@@ -6,58 +6,59 @@ import GetAppIcon from "@material-ui/icons/GetApp";
 export const Dashboard = () => {
   return (
     <div>
-      <Grid container spacing={3}>
-        {/* <Grid item xs={1}></Grid> */}
-        <Grid item xs={7}>
-          <div>
+      <div>
+        <Grid container spacing={3}>
+          <Grid item xs={7}>
             <div>
-              <div className="seftIntro">
+              <div>
+                <div className="seftIntro">
+                  <Typography
+                    className="seftIntro"
+                    align="left"
+                    paragraph={true}
+                    variant="h3"
+                    gutterBottom
+                  >
+                    Hey! I'm Nayan Pimpare. <br />
+                    I'm a Software Developer.
+                  </Typography>
+                </div>
                 <Typography
-                  className="seftIntro"
                   align="left"
                   paragraph={true}
-                  variant="h3"
+                  variant="h5"
                   gutterBottom
+                  style={{
+                    color: "#868e96",
+                    fontWeight: 400,
+                    marginBottom: "2.5rem",
+                    lineHeight: 1.6,
+                  }}
                 >
-                  Hey! I'm Nayan Pimpare. <br />
-                  I'm a Software Developer.
+                  This website is my <span>🌱</span> digital garden—a compendium
+                  of the things I have learned and created over the years, and
+                  anything else I want to write about. You can read my blog,
+                  view my guides & tutorials, or learn more about me.
                 </Typography>
               </div>
-              <Typography
-                align="left"
-                paragraph={true}
-                variant="h5"
-                gutterBottom
-                style={{
-                  color: "#868e96",
-                  fontWeight: 400,
-                  marginBottom: "2.5rem",
-                  lineHeight: 1.6,
-                }}
-              >
-                This website is my <span>🌱</span> digital garden—a compendium
-                of the things I have learned and created over the years, and
-                anything else I want to write about. You can read my blog, view
-                my guides & tutorials, or learn more about me.
-              </Typography>
             </div>
-          </div>
-        </Grid>
-        <Grid item xs={5}>
-          <div className="marginTop-10">
-            <div>
-              <img
-                className="dashboardImage"
-                src="/Photos/ProfilePhoto.png"
-                alt="ProfilePhoto"
-              />
+          </Grid>
+          <Grid item xs={5}>
+            <div className="marginTop-10">
+              <div>
+                <img
+                  className="dashboardImage"
+                  src="/Photos/ProfilePhoto.png"
+                  alt="ProfilePhoto"
+                />
+              </div>
             </div>
-          </div>
+          </Grid>
         </Grid>
-      </Grid>
+      </div>
       <div style={{ marginTop: "-11%" }}>
-        <Grid container>
-          <Grid xs={5}>
+        <Grid container spacing={3}>
+          <Grid xs={12}>
             <Button
               variant="contained"
               size="large"
@@ -83,7 +84,7 @@ export const Dashboard = () => {
               </a>
             </Button>
           </Grid>
-          <Grid xs={7}></Grid>
+          {/* <Grid xs={7}></Grid> */}
         </Grid>
       </div>
     </div>
